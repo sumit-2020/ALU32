1fd2123cf1b3cb7bf2f0d008fa4d069f0d0522c5
Sumit Kumar Yadav <sumit1828.umt2015@iitr.ac.in>
2016-12-08 19:53:15 +0530

    Adds program generator for unsigned array multiplier

---
+++ Unsigned_Array_Multiplier/mult.py
@@ -0,0 +1,20 @@
+def gen(q):
+	d=""
+	for i in range(0,q):
+		d=d+"0"
+	return d
+for i in range(1,16):
+	j=str(i)
+	k=str(16-i)
+	d="\twire [31:0] p"+j+" ;\n\tassign p"+j+" = {"+k+"'b"+gen(16-i)+",md[15:0]&{15{mr["+j+"]}},"+j+"'b"+gen(i)+"};"
+	print d
+#
+#for i in range(1,16):
+#	j=str(i)
+#	k=str(i-1)
+#	d="\tCSkipA cskipa"+j+"(s[" + k +"], p[" + j + "], s[" + j + "]);"
+#	print d
+#for i in range(0,15):
+#	j=str(i)
+#	d="\twire [31:0] s"+j+";"
+#	print d
