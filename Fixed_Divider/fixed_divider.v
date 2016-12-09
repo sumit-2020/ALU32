dedc7c210480e5be6e09d40f91cd382190026b2e
vinamarora8 <vinam.arora8@gmail.com>
2016-12-09 14:00:54 +0530

    Converted Fixed Divider 8/4 -> 32/16 bits

--- Fixed_Divider/fixed_divider.v
+++ Fixed_Divider/fixed_divider.v
@@ -2,47 +2,47 @@ module fixed_divider(inDividend, inDivisor, init, clock, Quotient, Remainder
 	);
 
 	// Assigning ports as in/out
-	input [7:0] inDividend;
-	input [3:0] inDivisor;
+	input [31:0] inDividend;
+	input [15:0] inDivisor;
 	input init, clock;
-	output [7:0] Quotient;
-	output [3:0] Remainder;
+	output [31:0] Quotient;
+	output [15:0] Remainder;
 
 	// Initialising required registers
-	reg [11:0] Dividend;
-	reg [7:0] Q;
-	reg [3:0] count;
+	reg [47:0] Dividend;
+	reg [31:0] Q;
+	reg [5:0] count;
 
 	// Initialising required wires
-	wire [4:0] diff = Dividend[11:7] - {1'b0, inDivisor};
-	wire Q_n	= ~diff[4];
-	wire [4:0] Dividend_n = (diff[4]) ? Dividend[11:7] : diff;
+	wire [16:0] diff = Dividend[47:31] - {1'b0, inDivisor};
+	wire Q_n	= ~diff[16];
+	wire [16:0] Dividend_n = (diff[16]) ? Dividend[47:31] : diff;
 	
 	// Assigning outputs
 	assign Quotient = Q;
-	assign Remainder = Dividend[11:8];
+	assign Remainder = Dividend[47:32];
 	
 	// Main sequential logic
 	always @(posedge clock)
 	begin
 		if (init == 1)
 		begin
-			Dividend = {4'b0000, inDividend};
-			Q = 8'b00000000;
-			count = 4'b0000;
+			Dividend = {16'b0000000000000000, inDividend};
+			Q = 32'b0000000000000000000000;
+			count = 6'b000000;
 		end
 
 		else if (init == 0)
 		begin
-			if (clock == 1 & count < 4'b1000)
+			if (clock == 1 & count < 6'b100000)
 			begin
-				Q = {Q[6:0], Q_n};
-				Dividend [11:7] = Dividend_n;	
+				Q = {Q[30:0], Q_n};
+				Dividend [47:31] = Dividend_n;	
 //			end
 //			else if (clock == 0 && count < 3'b100)
 //			begin
-				Dividend = {Dividend[10:0], 1'b0};
-				count = count + 4'b0001;
+				Dividend = {Dividend[46:0], 1'b0};
+				count = count + 6'b000001;
 			end
 		end
 	end
